054601205884712fc9527f3c05be0467d7bcd296
Stevie Gillen <stevie_gillen@hotmail.com>
2026-08-21 15:53:58 +0100

    Add collisions to keep player within game area

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var direction := Vector2.ZERO
 var speed := 50
 
+
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
