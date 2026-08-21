fe363d2d4064bfcb09179225c915b4d93754921e
Stevie Gillen <stevie_gillen@hotmail.com>
2026-08-21 15:20:07 +0100

    Add tree object and update to use physics for collisions

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,9 +1,9 @@
-extends Node2D
+extends CharacterBody2D
 
 var direction := Vector2.ZERO
-var speed := 1
+var speed := 50
 
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-
-	position += direction * speed
+	velocity = direction * speed
+	move_and_slide()
