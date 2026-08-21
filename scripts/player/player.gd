58d0ca7a3dec8723ae5fa2f9ae509f256138e0a0
Stevie Gillen <stevie_gillen@hotmail.com>
2026-08-21 16:21:53 +0100

    Add animations for player movement

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,7 +4,21 @@ var direction := Vector2.ZERO
 var speed := 50
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
+	animation()
 	move_and_slide()
+
+func animation():
+	if direction:
+		$AnimatedSprite2D.flip_h = direction.x > 0
+		if direction.x != 0:
+			$AnimatedSprite2D.animation = "walk_left"
+		else:
+			if direction.y > 0:
+				$AnimatedSprite2D.animation = "walk_down"
+			else:
+				$AnimatedSprite2D.animation = "walk_up"
+	else:
+		$AnimatedSprite2D.frame = 0
